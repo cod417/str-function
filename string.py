0628b84f90ed923ae4ecb5fe8ca6ba8c3d74d153
Balbir kumar <87017286+cod417@users.noreply.github.com>
2023-07-23 15:46:24 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,37 @@
+# string INdex
+
+# print("Hello"[4])
+# # oncatenate
+# print("1234" + "345")
+# # Boolean
+# True
+# False
+# # type Conversion and type casting
+# num_char = len(input("what is your name?"))
+
+# new_num_char = str(num_char)
+ 
+# print("your name has" + new_num_char +"characters.")
+
+# a = 1234
+# print(type(a))
+
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+# first_digit =int( two_digit_number[0])
+# second_digit = int(two_digit_number[1])
+
+# print(first_digit + second_digit)
+
+
+first_digit =int( two_digit_number[0])
+second_digit = int(two_digit_number[1])
+
+# print(first_digit + second_digit)
+result = int(first_digit ) + int(second_digit)
+print(result)
+
